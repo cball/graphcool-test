9b14cbc907c1181953296b58e91d287964f73b19
Chris Ball <chris@echobind.com>
2017-11-03 14:25:21 -0400

    add the rest

--- api/src/setBirthDate.js
+++ api/src/setBirthDate.js
@@ -1,7 +1,7 @@
 export default async event => {
   const data = { ...event.data };
 
-  data.dateOfBirth = data.dateOfBirth || new Date();
+  data.dateOfBirth = data.dateOfBirth || new Date(1999, 1, 1);
 
   return {
     data
